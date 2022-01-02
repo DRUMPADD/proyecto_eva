edb17bc0d9b052b6a8f7870348a95039ba8f52ff
Erick <erickfco1999m@gmail.com>
2022-01-01 18:47:02 -0600

    cors

--- src/main.js
+++ src/main.js
@@ -3,18 +3,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.set('port', process.env.PORT || 5000);
 
-// Middlewares
+// Requests
+app.use(cors({ origin: true }));
+
+// render files
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
-// ??
+// middlewares
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require("./routes"));
@@ -23,6 +29,4 @@ app.listen(app.get('port'), function () {
     console.log("Server listening on port", app.get('port'));
 });
 
-exports.modules = {
-    app
-}
+exports.widgets = functions.https.onRequest(app);
